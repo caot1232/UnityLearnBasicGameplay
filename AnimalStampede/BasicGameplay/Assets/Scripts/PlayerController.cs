e1dc5864f5d52dc964b3c399b7ae5e99b48f5647
theresa <caot@roverkids.org>
2025-12-11 13:42:37 -0500

    complete 2.2

--- AnimalStampede/BasicGameplay/Assets/Scripts/PlayerController.cs
+++ AnimalStampede/BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projecttilePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,7 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x< -xRange)
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
@@ -23,8 +26,16 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-            horizontalInput = Input.GetAxis("Horizontal");
-            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projecttilePrefab, transform.position, projecttilePrefab.transform.rotation);
         }
+
+
+
     }
+}
 
